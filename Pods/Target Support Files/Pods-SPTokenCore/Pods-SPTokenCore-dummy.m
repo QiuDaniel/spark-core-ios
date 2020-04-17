a59b2888aaaa0f7084287afe196d16de7757e8b7
qiudan <qiudan@sparkpool.com>
2020-04-17 19:05:51 +0800

    update .gitignore

---
+++ Pods/Target Support Files/Pods-SPTokenCore/Pods-SPTokenCore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPTokenCore : NSObject
+@end
+@implementation PodsDummy_Pods_SPTokenCore
+@end
---
+++ Pods/Target Support Files/Pods-SPTokenCoreTests/Pods-SPTokenCoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPTokenCoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_SPTokenCoreTests
+@end
